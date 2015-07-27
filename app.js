c9c4d580fc621de7f9bed03a03cd82d3b82f82aa
rjmk <rafekennedy@gmail.com>
2015-07-27 17:12:37 +0100

    google auth started

--- app.js
+++ app.js
@@ -10,6 +10,32 @@ server.connection({
 
 server.route(routes);
 
+server.register(require('bell'), function (err) {
+
+    // Declare an authentication strategy using the bell scheme
+    // with the name of the provider, cookie encryption password,
+    // and the OAuth client credentials.
+    server.auth.strategy('google', 'bell', {
+        provider: 'google',
+        password: 'cookie_encryption_password',
+        clientId: process.env.GOOGLE_ID,
+        clientSecret: process.env.GOOGLE_SECRET
+    });
+
+    server.route({
+        method: ['GET', 'POST'],
+        path: '/login',
+        config: {
+            auth: 'google',
+            handler: function (request, reply) {
+                console.log(request, reply);
+                return reply.redirect('/');
+            }
+        }
+    });
+
+});
+
 server.start(function () {
   console.log(server.info.uri);
 });
