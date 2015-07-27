3990562629f67297f229932991ab5149b86b27a1
rjmk <rafekennedy@gmail.com>
2015-07-27 15:34:31 +0100

    npm test mongos

--- test/_mongo_handlers.js
+++ test/_mongo_handlers.js
@@ -1,7 +1,7 @@
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 
-var url = 'mongodb://localhost:27017/prolifiko';
+var url = 'mongodb://localhost:27018/prolifiko';
 
 MongoClient.connect(url, function (err, db) {
 
