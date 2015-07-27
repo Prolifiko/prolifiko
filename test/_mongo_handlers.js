e0b44e22ac6a487789cca407f4213e21e0ac4090
rjmk <rafekennedy@gmail.com>
2015-07-27 14:58:27 +0100

    sample test

---
+++ test/_mongo_handlers.js
@@ -0,0 +1,35 @@
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
+var url = 'mongodb://localhost:27017/prolifiko';
+
+MongoClient.connect(url, function (err, db) {
+
+  assert.equal(null, err);
+
+  db.close();
+});
+
+var insertDocuments = function (db, callback) {
+  var collection = db.collection('users');
+  collection.insert([
+    {userId: 1, stars: {star1 : false}, timestamps: [], lastLogin: null},
+  ], function (err, result) {
+    assert.equal(err, null);
+    assert.equal(1, result.result.n);
+    assert.equal(1, result.ops.length);
+    console.log(result.ops);
+    callback(result);
+  });
+};
+
+MongoClient.connect(url, function (err, db) {
+
+  assert.equal(null, err);
+  console.log("Connected correctly to server");
+
+
+  insertDocuments(db, function() {
+    db.close();
+  });
+});
