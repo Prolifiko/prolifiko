a26279df540c6a291b0d87fc32e255477f000b77
Anni Vaananen <anni.vaananen@gmail.com>
2015-07-27 14:00:22 +0100

    added home route & a test for it

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var Hapi = require('hapi');
 var server = new Hapi.Server();
+var routes = require('./api/routes.js');
 
 server.connection({
   host: '0.0.0.0',
   port: Number(process.env.PORT) || 8000
 });
+
+server.route(routes);
 
 server.start(function () {
   console.log(server.info.uri);
