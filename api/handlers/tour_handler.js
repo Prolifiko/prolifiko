1113b7ebeceeb72390715d1470f152548ff63da3
rjmk <rafekennedy@gmail.com>
2015-07-30 17:16:06 +0100

    preparing content handlers

--- api/handlers/tour_handler.js
+++ api/handlers/tour_handler.js
@@ -3,14 +3,21 @@ var mongo = require('./mongo_content');
 function tour_handler (request, reply){
   mongo.findContent('tour', function(err, data){
     var button = 'Continue';
-    stage = request.params.stage || 0;
+    var stage = request.params.stage || 0;
+    var content = data.content[stage];
     var nextStep = '/tour/' + (+stage + 1);
+    var src = content.screenshot ? '/' + content.screenshot : false;
     if (+stage + 1 === data.content.length) {
       button = 'Sign Up';
       nextStep = '/progress';
     }
-
-    reply.view('tour', {tourContent: data.content[stage].text, nextStep: nextStep, button: button, src: '/'+data.content[stage].screenshot });
+    reply.view('tour', {
+      tourContent: content.text,
+      nextStep: nextStep,
+      button: button,
+      src: src,
+      title: content.title,
+    });
   });
 }
 
