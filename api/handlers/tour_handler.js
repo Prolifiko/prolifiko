67bb54fee2e136ddccd9b15323dc93ff8d976ab7
rjmk <rafekennedy@gmail.com>
2015-07-30 16:49:03 +0100

    tour functional

--- api/handlers/tour_handler.js
+++ api/handlers/tour_handler.js
@@ -2,8 +2,15 @@ var mongo = require('./mongo_content');
 
 function tour_handler (request, reply){
   mongo.findContent('tour', function(err, data){
+    var button = 'Continue';
     stage = request.params.stage || 0;
-    reply.view('tour', {tour_content: data.content[stage].text});
+    var nextStep = '/tour/' + (+stage + 1);
+    if (+stage + 1 === data.content.length) {
+      button = 'Sign Up';
+      nextStep = '/progress';
+    }
+
+    reply.view('tour', {tourContent: data.content[stage].text, nextStep: nextStep, button: button, src: '/'+data.content[stage].screenshot });
   });
 }
 
