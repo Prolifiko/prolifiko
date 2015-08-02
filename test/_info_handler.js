77a52d7cabae83f875b407c45f1f00cc5d08cec0
Anni Vaananen <anni.vaananen@gmail.com>
2015-08-02 20:10:54 +0100

    privacy policy content test etc.

--- test/_info_handler.js
+++ test/_info_handler.js
@@ -2,13 +2,22 @@ var test = require('tape');
 var server = require('../api/server.js');
 var fs = require('fs');
 
-test('The static handler return the terms page as expected', function (t) {
+test('The ifno handler return the terms page as expected', function (t) {
   server.inject({method:'GET', url:'/info/terms.html'},function (response) {
     var page = fs.readFileSync(__dirname + '/../public/html/terms.html', 'utf-8');
     t.equal(isInBody(response.result, page), true);
     t.end();
   });  
 });
+
+test('The info handler return the privacy policy page as expected', function (t) {
+  server.inject({method:'GET', url:'/info/policy.html'},function (response) {
+    var page = fs.readFileSync(__dirname + '/../public/html/policy.html', 'utf-8');
+    t.equal(isInBody(response.result, page), true);
+    t.end();
+  });  
+});
+
 
 function isInBody (x, y) {
   return x.indexOf(y) !== -1;
