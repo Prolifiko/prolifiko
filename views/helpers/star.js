82bf27af2c69d7095627920f0a492f9e36e77441
rjmk <rafekennedy@gmail.com>
2015-08-02 17:19:26 +0100

    all stars should be lookin good, tips and bonus need behavioural improvement

--- views/helpers/star.js
+++ views/helpers/star.js
@@ -1,6 +1,6 @@
 module.exports = function (context) {
   if (context.data.root.star){
     var step = context.data.root.step;
-    return '<button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect starButton" onclick="getMe.starPush('+ step + ')">';
+    return '<button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect starButton" id='+step+' onclick="getMe.starPush('+ step + ')">';
   }
 };
