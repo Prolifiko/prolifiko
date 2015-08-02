77479c93dce6a5cfdcb0a6ee864647f4cf382854
Anni Vaananen <anni.vaananen@gmail.com>
2015-08-02 17:51:17 +0100

    tips starfunction working

--- views/helpers/star.js
+++ views/helpers/star.js
@@ -1,6 +1,7 @@
 module.exports = function (context) {
   if (context.data.root.star){
     var step = context.data.root.step;
-    return '<button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect starButton" onclick="getMe.starPush('+ step + ')">';
+    step= typeof step === 'number'? step: "'" +step+ "'";
+    return '<button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect starButton" onclick="getMe.starPush('+ step + ')" />';
   }
 };
