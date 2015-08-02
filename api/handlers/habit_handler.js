90b45f238afdcddc2db9db65daa26b2f54b52aef
Anni Vaananen <anni.vaananen@gmail.com>
2015-08-02 13:53:57 +0100

    habit handler done

---
+++ api/handlers/habit_handler.js
@@ -0,0 +1,23 @@
+var mongo = require('./mongo_content');
+
+function contentHandler (step) {
+  return function (request, reply) {
+    mongo.findContent('stepHabit', function(err, data){
+      var button = 'My Progress';
+      var stage = request.params.stage || 0;
+      var content = data.content[stage];
+      var next = "/progress";
+
+      reply.view('habit', {
+        button: button,
+        text: content.text,
+        next: next,
+        title: content.title,
+        step: step,
+        title_header: data.titleHeader
+      });
+    });
+  };
+}
+
+module.exports = contentHandler;
